f6ecc0482b7bb6d4f47a8a7583974229f5b58e21
Jan Kotanski <jankotan@gmail.com>
2024-04-12 12:09:24 +0200

    Issue 125: add noble tests (#126)
    * add noble tests
    
    * trigger the tests
    
    * add missing file
    
    * ser missing nxsconfig DB

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # workaround for a bug in debian9, i.e. starting mysql hangs
-if [ "$1" = "debian11" ] || [ "$1" = "debian12" ]; then
+if [ "$1" = "debian11" ] || [ "$1" = "debian12" ] || [ "$1" = "ubuntu24.04" ]; then
     docker exec --user root ndts service mariadb restart
 else
     docker exec --user root ndts service mysql stop
@@ -12,30 +12,71 @@ else
     # docker exec  --user root ndts /bin/bash -c '$(service mysql start &) && sleep 30'
 fi
 
-docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get -qq install -y   tango-db tango-common; sleep 10'
-if [ "$?" != "0" ]; then exit 255; fi
-
+echo "install tango-common"
+docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y tango-common; sleep 10'
+if  [ "$1" = "ubuntu24.04" ]; then
+    # docker exec  --user tango ndts /bin/bash -c '/usr/lib/tango/DataBaseds 2 -ORBendPoint giop:tcp::10000  &'
+    docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=root\npassword=rootpw" > /root/.my.cnf'
+    docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=tango\nhost=localhost\npassword=rootpw" > /var/lib/tango/.my.cnf'
+    docker exec  --user root ndts /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON tango.* TO "tango"@"%" identified by "rootpw"'
+    docker exec  --user root ndts /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON tango.* TO "tango"@"localhost" identified by "rootpw"'
+    docker exec  --user root ndts /usr/bin/mysql -e 'FLUSH PRIVILEGES'
+fi
 if [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "ubuntu21.04" ] || [ "$1" = "ubuntu21.10" ] || [ "$1" = "ubuntu22.04" ]; then
     # docker exec  --user tango ndts /bin/bash -c '/usr/lib/tango/DataBaseds 2 -ORBendPoint giop:tcp::10000  &'
     docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=root\npassword=rootpw" > /root/.my.cnf'
     docker exec  --user root ndts /bin/bash -c 'echo -e "[client]\nuser=tango\nhost=127.0.0.1\npassword=rootpw" > /var/lib/tango/.my.cnf'
 fi
+echo "install tango-db"
+docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive; apt-get -qq install -y tango-db; sleep 10'
+if [ "$?" -ne "0" ]; then exit 255; fi
+if  [ "$1" = "ubuntu24.04" ]; then
+    docker exec  --user tango ndts /usr/bin/mysql -e 'create database tango'
+    docker exec  --user tango ndts /bin/bash -c '/usr/bin/mysql tango < /usr/share/dbconfig-common/data/tango-db/install/mysql'
+fi
+
 docker exec  --user root ndts service tango-db restart
 
+
 echo "install tango servers"
-docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get -qq install -y  tango-starter tango-test'
-if [ "$?" != "0" ]; then exit 255; fi
+docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; export DEBIAN_FRONTEND=noninteractive;  apt-get -qq install -y  tango-starter tango-test liblog4j1.2-java'
+if [ "$?" -ne "0" ]; then exit 255; fi
 
 docker exec --user root ndts service tango-starter restart
-docker exec --user root ndts chown -R tango:tango .
+
+docker exec  --user root ndts chown -R tango:tango .
 
 
-echo "install python3-pytango and nxs packages"
+if [ "$2" = "2" ]; then
+    echo "install pytango and nxsconfigserver-db"
+    docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; apt-get install -y   python-pytango  nxsconfigserver-db ; sleep 10'
+else
+    if [ "$1" = "debian10" ] || [ "$1" = "ubuntu24.04" ] || [ "$1" = "ubuntu22.04" ] || [ "$1" = "ubuntu20.04" ] || [ "$1" = "ubuntu20.10" ] || [ "$1" = "debian11" ] || [ "$1" = "debian12" ] ; then
+	echo "install pytango"
+	docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get install -y   python3-tango'
+	echo "install nxsconfigserver-db"
+	docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get  install -y   nxsconfigserver-db'
+	if [ "$1" = "ubuntu24.04" ]; then
+	    docker exec  --user root ndts /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON nxsconfig.* TO "tango"@"%" identified by "rootpw"'
+	    docker exec  --user root ndts /usr/bin/mysql -e 'GRANT ALL PRIVILEGES ON nxsconfig.* TO "tango"@"localhost" identified by "rootpw"'
+	    docker exec  --user root ndts /usr/bin/mysql -e 'FLUSH PRIVILEGES'
+	    docker exec  --user tango ndts /usr/bin/mysql -e 'create database nxsconfig'
+	    docker exec  --user tango ndts /bin/bash -c '/usr/bin/mysql nxsconfig < /usr/share/dbconfig-common/data/nxsconfigserver-db/install/mysql'
+	fi
+    else
+	echo "install pytango and nxsconfigserver-db"
+	docker exec  --user root ndts /bin/bash -c 'apt-get -qq update; apt-get -qq install -y   python3-pytango nxsconfigserver-db; sleep 10'
+    fi
+fi
+if [ "$?" != "0" ]; then exit 255; fi
+
+
+echo "install nxs packages"
 if [ "$2" = "2" ]; then
     echo "install python-pytango"
-    docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get install -y python-pytango nxsconfigserver-db python-nxsconfigserver python-nxswriter python-nxstools; sleep 10'
+    docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get install -y python-nxsconfigserver python-nxswriter python-nxstools; sleep 10'
 else
-    docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get install -y python3-tango nxsconfigserver-db python3-nxsconfigserver python3-nxswriter python3-nxstools; sleep 10'
+    docker exec --user root ndts /bin/bash -c 'apt-get -qq update; apt-get install -y python3-nxsconfigserver python3-nxswriter python3-nxstools; sleep 10'
 fi
 if [ "$?" != "0" ]; then exit 255; fi
 
